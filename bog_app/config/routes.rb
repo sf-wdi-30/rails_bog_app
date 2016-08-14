5078feead74b8a8d7fbbcaa66f262916dd972e3c
mattericbrown <matt.eric.brown@gmail.com>
2016-08-13 17:06:09 -0700

    made routes and methods for update and edit

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "creatures#index"
 
-  resources :creatures, only: [:index, :new, :create, :show]
+  resources :creatures, only: [:index, :new, :create, :show, :edit, :update]
 
 end
